033bf1aab436aec3463c658cfc2d56bba4e539bf
oleggr <oleg.gr@outlook.com>
2022-05-08 19:19:33 +0300

    delete buttons updated

--- frontend/src/components/DragAndDropForm.vue
+++ frontend/src/components/DragAndDropForm.vue
@@ -16,7 +16,7 @@
       <div>or browse</div>
       <div v-if="File.length">
         <ul v-for="(file, index) in File" :key="file" :index="index">
-          <li>{{file.name}} <button v-on:click="()=>deleteHandler(index)">X</button></li>
+          <li>{{file.name}} <button v-on:click="()=>deleteHandler(index)" class="google-button-cross">X</button></li>
         </ul>
       </div>
     </div>
@@ -148,6 +148,46 @@ export default {
   .google-button:hover::before {
     opacity: .1;
     transform: scale(2);
+  }
+
+  .google-button-cross {
+    align-items: center;
+    border: none;
+    display: inline-flex;
+    justify-content: center;
+    outline: none;
+    position: relative;
+    z-index: 0;
+    -webkit-font-smoothing: antialiased;
+    background: none;
+    border-radius: 4px;
+    cursor: pointer;
+    padding: 0 8px;
+    white-space: pre-wrap;
+    font-size : 20px; width: 5%;
+  }
+
+  .google-button-cross::before {
+    content: '';
+    display: block;
+    opacity: 0;
+    position: absolute;
+    transition-duration: .15s;
+    transition-timing-function: cubic-bezier(0.4,0.0,0.2,1);
+    z-index: -1;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    background: #25609c;
+    border-radius: 4px;
+    transform: scale(0);
+    transition-property: transform,opacity;
+  }
+
+  .google-button-cross:hover::before {
+    opacity: .1;
+    transform: scale(1);
   }
 
   .request_input {
