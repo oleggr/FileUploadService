b2c4e6c2c483c340b3d068f4fd9925e8391a1428
oleggr <oleg.gr@outlook.com>
2022-07-14 12:54:51 +0300

    small fixes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,8 @@ module.exports = defineConfig({
   outputDir: "../backend/html/",
   assetsDir: process.env.NODE_ENVIRON === "prod"
     ? "../html"
-    : ""
+    : "",
+  head: [
+    ['link', { rel: "icon", type: "image/png", href: "/public/favicon-32x32.png"}],
+  ],
 })
